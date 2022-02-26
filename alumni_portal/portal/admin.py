630a78aaab36e455281ed46966febb0de24fc6d7
vishaldev-tech <73268978+vishaldev-tech@users.noreply.github.com>
2022-02-26 23:10:20 +0530

    Improved version of the project Alumni Portal

--- alumni_portal/portal/admin.py
+++ alumni_portal/portal/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from portal.models import Batch, Country, Department, District, Experience_Detail, Mentor_Post, Mentor_Post_Response, Mentor_Response_Message, Post, Post_Education_Detail, Post_Response, Response_Message, Sponser, State, Student_Support, Tag, User
+from portal.models import Batch, Country, Department, District, ExperienceDetail, MentorPost, MentorPostResponse, MentorResponseMessage, Post, PostEducationDetail, PostResponse, ResponseMessage, Sponser, State, User
 
 # Register your models here.
 
@@ -10,13 +10,12 @@ admin.site.register(State)
 admin.site.register(District)
 admin.site.register(Batch)
 admin.site.register(Department)
-admin.site.register(Post_Education_Detail)
-admin.site.register(Experience_Detail)
+admin.site.register(PostEducationDetail)
+admin.site.register(ExperienceDetail)
 admin.site.register(Post)
-admin.site.register(Post_Response)
-admin.site.register(Response_Message)
-admin.site.register(Mentor_Post)
-admin.site.register(Mentor_Post_Response)
-admin.site.register(Mentor_Response_Message)
-admin.site.register(Student_Support)
+admin.site.register(PostResponse)
+admin.site.register(ResponseMessage)
+admin.site.register(MentorPost)
+admin.site.register(MentorPostResponse)
+admin.site.register(MentorResponseMessage)
 admin.site.register(Sponser) 
