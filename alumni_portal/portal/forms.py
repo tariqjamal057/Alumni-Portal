4da54b5dae5fd4a47484bdad8a5e120d69eb5061
vishaldev-tech <73268978+vishaldev-tech@users.noreply.github.com>
2022-03-30 14:15:22 +0530

    Update

--- alumni_portal/portal/forms.py
+++ alumni_portal/portal/forms.py
@@ -1,11 +1,17 @@
 
 from django.forms import ModelForm
-from .models import Finance_request, Mark
+from .models import Filter_Finance, Finance_request, Mark
 
 class FinanceHelpForm(ModelForm):
     class Meta:
         model = Finance_request
         fields = ['title','image','student_name','description','amount','marks','achievements','deadline']
+
+class Filterfinance(ModelForm):
+    class Meta:
+        model = Filter_Finance
+        fields = ['date','amount']
+
 class MarkForm(ModelForm):
     class Meta:
         model = Mark
