3c71f898af3b0060ae60eec42cbca586ea1a8976
Tariq Jamal A <tariqjamal4267324@gmail.com>
2024-09-14 23:30:54 +0530

    admin static files

--- alumni_portal/settings.py
+++ alumni_portal/settings.py
@@ -128,6 +128,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+]
+
+
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
