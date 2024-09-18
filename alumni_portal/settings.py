099a9e4eefbfa2c0fa756daddc02ae2e40d173cb
Tariq Jamal A <tariqjamal4267324@gmail.com>
2024-09-18 22:24:25 +0530

    refactor: integerate formatter

--- alumni_portal/settings.py
+++ alumni_portal/settings.py
@@ -16,82 +16,81 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 BASE_DIR2 = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-TEMPLATE_DIR = os.path.join(BASE_DIR2,'templates')
-STATIC_DIR = os.path.join(BASE_DIR2, 'static')
+TEMPLATE_DIR = os.path.join(BASE_DIR2, "templates")
+STATIC_DIR = os.path.join(BASE_DIR2, "static")
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'portal',
-    'ckeditor',
-    'ckeditor_uploader',
-    'crispy_forms',
-    'django_social_share',
-    'import_export',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "portal",
+    "ckeditor",
+    "ckeditor_uploader",
+    "crispy_forms",
+    "django_social_share",
+    "import_export",
 ]
 
 # CKEDITOR_UPLOADER_PATH = 'media/'
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'alumni_portal.urls'
+ROOT_URLCONF = "alumni_portal.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [os.path.join(BASE_DIR, "templates")],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'alumni_portal.wsgi.application'
+WSGI_APPLICATION = "alumni_portal.wsgi.application"
 
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
     }
 }
-
 
 
 # Password validation
@@ -99,16 +98,16 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
@@ -116,9 +115,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'Asia/Kolkata'
+TIME_ZONE = "Asia/Kolkata"
 
 USE_I18N = True
 
@@ -129,49 +128,47 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATICFILES_FINDERS = [
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
 ]
 
 
-STATIC_URL = '/static/'
+STATIC_URL = "/static/"
 
-STATICFILES_DIRS = [
-    STATIC_DIR
-]
+STATICFILES_DIRS = [STATIC_DIR]
 
 STATIC_ROOT = os.path.join(BASE_DIR, "/static")
 
-MEDIA_URL = '/uploads/'
-MEDIA_ROOT = os.path.join(BASE_DIR2, 'uploads')
+MEDIA_URL = "/uploads/"
+MEDIA_ROOT = os.path.join(BASE_DIR2, "uploads")
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
-AUTH_USER_MODEL = 'portal.User'
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = "portal.User"
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-LOGIN_REDIRECT_URL = 'dashboard'
+LOGIN_REDIRECT_URL = "dashboard"
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 CKEDITOR_UPLOAD_PATH = "uploads/"
 CKEDITOR_CONFIGS = {
-    'default':
-        {
-            'toolbar': 'full',
-            'width': 'auto',
-            'extraPlugins': ','.join([
-                'codesnippet',
-            ]),
-        },
+    "default": {
+        "toolbar": "full",
+        "width": "auto",
+        "extraPlugins": ",".join(
+            [
+                "codesnippet",
+            ]
+        ),
+    },
 }
 
 
-EMAIL_ADMIN = os.environ.get('EMAIL_ADMIN')
+EMAIL_ADMIN = os.environ.get("EMAIL_ADMIN")
 
-EMAIL_HOST = os.environ.get('EMAIL_HOST')
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
-EMAIL_PORT = os.environ.get('EMAIL_PORT')
-
+EMAIL_HOST = os.environ.get("EMAIL_HOST")
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+EMAIL_PORT = os.environ.get("EMAIL_PORT")
--- portal/admin.py
+++ portal/admin.py
@@ -1,101 +1,140 @@
 from django.contrib import admin
-from import_export.admin import ImportExportModelAdmin
-from portal.models import *
 from django.contrib.auth.hashers import make_password
+from import_export.admin import ImportExportModelAdmin
 
-admin.site.site_header = 'Karpagam Institute of Technology'
-admin.site.site_title = 'Admin'
-admin.site.index_title = ''
+from portal.models import *
+
+admin.site.site_header = "Karpagam Institute of Technology"
+admin.site.site_title = "Admin"
+admin.site.index_title = ""
 
 
-class AdminConfig(ImportExportModelAdmin,admin.ModelAdmin):
-    list_display = ['id','username','email','registered_on']
+class AdminConfig(ImportExportModelAdmin, admin.ModelAdmin):
+    list_display = ["id", "username", "email", "registered_on"]
 
     def save_model(self, request, obj, form, change):
-        if 'password' in form.changed_data:
-            obj.password = make_password(form.cleaned_data.get('password'))
+        if "password" in form.changed_data:
+            obj.password = make_password(form.cleaned_data.get("password"))
             # obj.car.confirm =_('approved')
             # obj.car.save()
-        super().save_model(request, obj, form, change) 
-      
-admin.site.register(User,AdminConfig)
+        super().save_model(request, obj, form, change)
 
-class CountryConfig(ImportExportModelAdmin,admin.ModelAdmin):
-    list_display = ['id','name']
 
-admin.site.register(Country,CountryConfig)
+admin.site.register(User, AdminConfig)
 
-class StateConfig(ImportExportModelAdmin,admin.ModelAdmin):
-    list_display = ['id','name']
 
-admin.site.register(State,StateConfig)
+class CountryConfig(ImportExportModelAdmin, admin.ModelAdmin):
+    list_display = ["id", "name"]
 
-class DistrictConfig(ImportExportModelAdmin,admin.ModelAdmin):
-    list_display = ['id','name','state']
 
-admin.site.register(District,DistrictConfig)
+admin.site.register(Country, CountryConfig)
 
-class BatchConfig(ImportExportModelAdmin,admin.ModelAdmin):
-    list_display = ['id','entry_year','passed_out']
-admin.site.register(Batch,BatchConfig)
 
-class DepartmentConfig(ImportExportModelAdmin,admin.ModelAdmin):
-    list_display = ['id','short_name','name']
+class StateConfig(ImportExportModelAdmin, admin.ModelAdmin):
+    list_display = ["id", "name"]
 
-admin.site.register(Department,DepartmentConfig)
 
-class PostEducationDetailConfig(ImportExportModelAdmin,admin.ModelAdmin):
-    list_display = ['id','user','degree','institute_or_university','currently_pursing']
+admin.site.register(State, StateConfig)
 
-admin.site.register(PostEducationDetail,PostEducationDetailConfig)
 
-class ExperienceDetailConfig(ImportExportModelAdmin,admin.ModelAdmin):
-    list_display = ['id','user','designation','organization','currently_working_here']
+class DistrictConfig(ImportExportModelAdmin, admin.ModelAdmin):
+    list_display = ["id", "name", "state"]
 
-admin.site.register(ExperienceDetail,ExperienceDetailConfig)
 
-class PostConfig(ImportExportModelAdmin,admin.ModelAdmin):
-    list_display = ['id','title','posted_by','date']
-admin.site.register(Post,PostConfig)
+admin.site.register(District, DistrictConfig)
 
-class PostResponseConfig(ImportExportModelAdmin,admin.ModelAdmin):
-    list_display = ['id','user','post']
 
-admin.site.register(PostResponse,PostResponseConfig)
+class BatchConfig(ImportExportModelAdmin, admin.ModelAdmin):
+    list_display = ["id", "entry_year", "passed_out"]
 
-class ResponseMessageConfig(ImportExportModelAdmin,admin.ModelAdmin):
-    list_display = ['id','user','post_response','message','date']
 
-admin.site.register(ResponseMessage,ResponseMessageConfig)
+admin.site.register(Batch, BatchConfig)
 
-class Tech_Help_PostConfig(ImportExportModelAdmin,admin.ModelAdmin):
-    list_display = ['id','title','stack','posted_by','date']
 
-admin.site.register(Tech_Help_Post,Tech_Help_PostConfig)
+class DepartmentConfig(ImportExportModelAdmin, admin.ModelAdmin):
+    list_display = ["id", "short_name", "name"]
 
-class Tech_Help_PostResponseConfig(ImportExportModelAdmin,admin.ModelAdmin):
-    list_display = ['id','user','post']
 
-admin.site.register(Tech_Help_PostResponse,Tech_Help_PostResponseConfig)
+admin.site.register(Department, DepartmentConfig)
 
-class Tech_Help_ResponseMessageConfig(ImportExportModelAdmin,admin.ModelAdmin):
-    list_display = ['id','user','post_response','message','date']
 
-admin.site.register(Tech_Help_ResponseMessage,Tech_Help_ResponseMessageConfig)
+class PostEducationDetailConfig(ImportExportModelAdmin, admin.ModelAdmin):
+    list_display = ["id", "user", "degree", "institute_or_university", "currently_pursing"]
+
+
+admin.site.register(PostEducationDetail, PostEducationDetailConfig)
+
+
+class ExperienceDetailConfig(ImportExportModelAdmin, admin.ModelAdmin):
+    list_display = ["id", "user", "designation", "organization", "currently_working_here"]
+
+
+admin.site.register(ExperienceDetail, ExperienceDetailConfig)
+
+
+class PostConfig(ImportExportModelAdmin, admin.ModelAdmin):
+    list_display = ["id", "title", "posted_by", "date"]
+
+
+admin.site.register(Post, PostConfig)
+
+
+class PostResponseConfig(ImportExportModelAdmin, admin.ModelAdmin):
+    list_display = ["id", "user", "post"]
+
+
+admin.site.register(PostResponse, PostResponseConfig)
+
+
+class ResponseMessageConfig(ImportExportModelAdmin, admin.ModelAdmin):
+    list_display = ["id", "user", "post_response", "message", "date"]
+
+
+admin.site.register(ResponseMessage, ResponseMessageConfig)
+
+
+class Tech_Help_PostConfig(ImportExportModelAdmin, admin.ModelAdmin):
+    list_display = ["id", "title", "stack", "posted_by", "date"]
+
+
+admin.site.register(Tech_Help_Post, Tech_Help_PostConfig)
+
+
+class Tech_Help_PostResponseConfig(ImportExportModelAdmin, admin.ModelAdmin):
+    list_display = ["id", "user", "post"]
+
+
+admin.site.register(Tech_Help_PostResponse, Tech_Help_PostResponseConfig)
+
+
+class Tech_Help_ResponseMessageConfig(ImportExportModelAdmin, admin.ModelAdmin):
+    list_display = ["id", "user", "post_response", "message", "date"]
+
+
+admin.site.register(Tech_Help_ResponseMessage, Tech_Help_ResponseMessageConfig)
 
 admin.site.register(Featured_Mentor)
 
-class Finance_request_Config(ImportExportModelAdmin,admin.ModelAdmin):
-    list_display = ['id','title','name','year','department','posted_by']
-admin.site.register(Finance_request,Finance_request_Config)
 
-class Finance_request_Post_Response_Config(ImportExportModelAdmin,admin.ModelAdmin):
-    list_display = ['id','post','user']
-admin.site.register(Finance_request_Post_Response,Finance_request_Post_Response_Config)
+class Finance_request_Config(ImportExportModelAdmin, admin.ModelAdmin):
+    list_display = ["id", "title", "name", "year", "department", "posted_by"]
 
-class Finance_request_Response_Message_Config(ImportExportModelAdmin,admin.ModelAdmin):
-    list_display = ['id','user','post_response','message','date']
-admin.site.register(Finance_request_Response_Message,Finance_request_Response_Message_Config)
+
+admin.site.register(Finance_request, Finance_request_Config)
+
+
+class Finance_request_Post_Response_Config(ImportExportModelAdmin, admin.ModelAdmin):
+    list_display = ["id", "post", "user"]
+
+
+admin.site.register(Finance_request_Post_Response, Finance_request_Post_Response_Config)
+
+
+class Finance_request_Response_Message_Config(ImportExportModelAdmin, admin.ModelAdmin):
+    list_display = ["id", "user", "post_response", "message", "date"]
+
+
+admin.site.register(Finance_request_Response_Message, Finance_request_Response_Message_Config)
 
 # class Featured_Sponser_config(ImportExportModelAdmin,admin.ModelAdmin):
 #     list_display = ['id','student_name','user','amount','date']
