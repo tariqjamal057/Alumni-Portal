41907339cf68424fef8f5f76c829714432b07271
Tariq Jamal A <tariqjamal4267324@gmail.com>
2024-09-28 23:13:26 +0530

    refactor: add context public module class

--- portal/views/public.py
+++ portal/views/public.py
@@ -1,18 +1,27 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
 
+from portal.utils import is_alumni
 
-class Home(TemplateView):
+
+class BasePublicContext(TemplateView):
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["is_alumni"] = is_alumni(self.request.user)
+        return context
+
+
+class Home(BasePublicContext):
     template_name = "public/index.html"
 
 
-class About(TemplateView):
+class About(BasePublicContext):
     template_name = "public/about.html"
 
 
-class Gallery(TemplateView):
+class Gallery(BasePublicContext):
     template_name = "public/gallery.html"
 
 
-class Contact(TemplateView):
+class Contact(BasePublicContext):
     template_name = "public/contact.html"
