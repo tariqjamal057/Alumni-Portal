829f3c8ade7b4b88af757514a35d19d3c961ad6a
Tariq Jamal A <tariqjamal4267324@gmail.com>
2024-10-13 19:55:49 +0530

    refactor: add chat module

--- portal/templatetags/custom_tags.py
+++ portal/templatetags/custom_tags.py
@@ -1,5 +1,9 @@
 from django import template
 
+from portal.utils import is_alumni as alumni
+from portal.utils import is_faculty as faculty
+from portal.utils import is_student as student
+
 register = template.Library()
 
 
@@ -11,3 +15,18 @@ def add_class(field, css_class):
 @register.inclusion_tag("layouts/components/form_field.html")
 def render_form_field(field, form_class="form-control", label_class="sr-only"):
     return {"field": field, "form_class": form_class, "label_class": label_class}
+
+
+@register.filter
+def is_student(user):
+    return student(user)
+
+
+@register.filter
+def is_alumni(user):
+    return alumni(user)
+
+
+@register.filter
+def is_faculty(user):
+    return faculty(user)
