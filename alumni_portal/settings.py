cd297fb4567c9a0d5d31cd1aa7d56739009eacc9
Tariq Jamal A <tariqjamal4267324@gmail.com>
2024-10-13 22:11:09 +0530

    refactor: resolve static file issue

--- alumni_portal/settings.py
+++ alumni_portal/settings.py
@@ -27,7 +27,7 @@ STATIC_DIR = os.path.join(BASE_DIR2, "static")
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG")
 
 ALLOWED_HOSTS = ["*"]
 
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -138,6 +139,9 @@ STATICFILES_DIRS = [STATIC_DIR]
 STATIC_ROOT = os.path.join(BASE_DIR, "/static")
 MEDIA_URL = "/uploads/"
 MEDIA_ROOT = os.path.join(BASE_DIR2, "uploads")
+
+if not DEBUG:
+    STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
